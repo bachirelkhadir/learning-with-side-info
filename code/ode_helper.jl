767b21daeed861bd2fd706cd475f16218dba64b2
Bachir El Khadir <bachir009@gmail.com>
2021-11-14 14:57:40 -0500

    .

--- code/ode_helper.jl
+++ code/ode_helper.jl
@@ -11,8 +11,10 @@ function solve_ode(vf, x0; verbose=true, tspan = (0., 30.0), length=10)
     function vf_for_solver(dx, x, params, t)
         dx[1], dx[2] = vf(x[1], x[2])
     end
-    prob = ODEProblem(vf_for_solver, x0, tspan)
-    sol = DifferentialEquations.solve(prob)
+    prob = ODEProblem(vf_for_solver, x0, tspan, )
+    alg = Euler()
+    maxiters = 1e4
+    sol = DifferentialEquations.solve(prob, alg, dt=(tspan[2]-tspan[1])/maxiters)
     trange = range(tspan[1], stop=tspan[2], length=length)
     if verbose
         @show size(sol)
@@ -25,3 +27,24 @@ function solve_ode(vf, x0; verbose=true, tspan = (0., 30.0), length=10)
     return (sol[1, :], sol[2, :])
 end
 
+
+function solve_ode_3d(vf, x0; verbose=true, tspan = (0., 30.0), length=10)
+    if verbose
+        println("Solving ODE from x0=$x0")
+    end
+    function vf_for_solver(dx, x, params, t)
+        dx[1], dx[2], dx[3] = vf(x[1], x[2], x[3])
+    end
+    prob = ODEProblem(vf_for_solver, x0, tspan)
+    sol = DifferentialEquations.solve(prob)
+    trange = range(tspan[1], stop=tspan[2], length=length)
+    if verbose
+        @show size(sol)
+        @show length
+    end
+    if size(sol, 2) <= 1
+        throw(OdeDiverges())
+    end
+    sol = hcat(sol.(trange)...)
+    return (sol[1, :], sol[2, :], sol[3, :])
+end
